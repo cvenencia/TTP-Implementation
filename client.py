4942397cf08c079418a30e9e71a46fa8e310e3fd
Carlos Venencia <carlos.venencia@hotmail.com>
2022-05-15 12:08:55 -0500

    Commented code

--- client.py
+++ client.py
@@ -9,10 +9,8 @@ class Client:
     def __init__(self):
         self.id = randint(0, 2**128-1)
 
-def main():
-
-    X = Client()
-
+def registration_process(X):
+    # Connect to TTP on port 8000 to start registration
     conn_register = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     conn_register.connect(("127.0.0.1", 8000))
 
@@ -21,27 +19,36 @@ def main():
     # Receive kp and km from TTP
     X.k, X.km = [int(i) for i in conn_register.recv(2048).decode('utf-8').split('\n')]
 
-    X.r = get_random_bytes(12)
+def communication_process(X):
+    # Connect to TTP on port 8001 to start communication
     conn_comm = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     conn_comm.connect(("127.0.0.1", 8001))
 
+    # Generate nonce
+    X.r = get_random_bytes(12)
     
+    # Send ID and nonce to TTP
     conn_comm.sendall(str.encode("\n".join([str(X.id), str(int.from_bytes(X.r, "little"))])))
     
+    # Receive the information back from TTP
     cx, tx, idy, ry, n = [int(i) for i in conn_comm.recv(2048).decode('utf-8').split('\n')]
 
     if n == 0:
+        # If user is P
+
         cp = cx.to_bytes(32, "little")
         tp = tx.to_bytes(16, "little")
         idq = idy.to_bytes(16, "little")
         rq = ry.to_bytes(12, "little")
         
+        # 5.a. Verify Tx is a valid MAC on the message
         cobj = CMAC.new(X.km.to_bytes(16, "little"), ciphermod=AES)
         cobj.update(bytearray(idq+rq+X.r+cp))
-
         try:
             cobj.verify(tp)
             print ("The key  with partner %s wasn't altered "%str(idq)[1:] )
+
+            # 5.b. Decrypt key
             cipher = AES.new(X.k.to_bytes(32, "little"), AES.MODE_GCM, nonce=X.r)
             k= cipher.decrypt(cp)
             print ("Key: "+str(k))
@@ -49,22 +56,31 @@ def main():
             print ("The message or the key is wrong")
 
     elif n == 1:
+        # If user is Q
+
         cq = cx.to_bytes(32, "little")
         tq = tx.to_bytes(16, "little")
         idp = idy.to_bytes(16, "little")
         rp = ry.to_bytes(12, "little")
 
-
+        # 5.a. Verify Tx is a valid MAC on the message
         cobj = CMAC.new(X.km.to_bytes(16, "little"), ciphermod=AES)
         cobj.update(bytearray(idp+X.r+rp+cq))
-
         try:
             cobj.verify(tq)
             print ("The key  with partner %s wasn't altered "%str(idp)[1:] )
+
+            # 5.b. Decrypt key
             cipher = AES.new(X.k.to_bytes(32, "little"), AES.MODE_GCM, nonce=rp)
             k= cipher.decrypt(cq)
             print ("Key: "+str(k))
         except ValueError:
             print ("The message or the key is wrong")
+
+def main():
+    X = Client()
+
+    registration_process(X)
+    communication_process(X)
 
 main()
--- ttp.py
+++ ttp.py
@@ -28,15 +28,20 @@ def register_user(s):
     conn, _ = s.accept()
     with conn:
         try:
+            # Receive ID and generate key for user
             id = conn.recv(1024)
             key = get_random_bytes(32)
             keym = get_random_bytes(16)
+
+            # Add user to the table
             add_user({
                 "id": int.from_bytes(id, 'little'),
                 "key": int.from_bytes(key, 'little'),
                 "keym": int.from_bytes(keym, 'little')
             })
             # print(f"User with ID {int.from_bytes(id, 'little')} registered with key {int.from_bytes(key, 'little')}")
+            
+            # Send back key to user
             conn.sendall(str.encode("\n".join([
                 str(int.from_bytes(key, "little")),
                 str(int.from_bytes(keym, "little")),
@@ -47,15 +52,18 @@ def register_user(s):
 
 def register_requests_handler():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        # Registration requests are handled on port 8000
         s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         s.bind(("127.0.0.1", 8000))
         s.listen()
         print("Open port waiting for register requests...")
         while True:
+            # Always listen for registration requests
             register_user(s)
 
-def asdf(conn, user, return_dict):
+def communication_request(conn, user, return_dict):
     try:
+        # Receive ID and nonce from user
         idx, rx = [int(i) for i in conn.recv(2048).decode('utf-8').split('\n')]
         if user == 0:
             return_dict["idp"] = (idx).to_bytes(16, "little")
@@ -69,67 +77,84 @@ def asdf(conn, user, return_dict):
         if conn:
             conn.close()
 
-def asdf_handler():
+def communication_requests_handler():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        # Communication requests are handled on port 8001
         s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         s.bind(("127.0.0.1", 8001))
         s.listen()
         print("Open port waiting for communication requests...")
-        users_connected = 0
         
-        # while True:
-        processes = []
-        conns = []
-        manager = Manager()
-        data = manager.dict()
-        while users_connected < 2:
-            conn, _ = s.accept()
-            conns.append(conn)
-            p = Process(target=asdf, args=(conn, users_connected, data))
-            processes.append(p)
-            p.start()
-            users_connected += 1
-        
-        for p in processes:
-            p.join()
+        # Always listen for communication requests
+        while True:
+            users_connected = 0
+            processes = []
+            conns = []
+            manager = Manager()
+            data = manager.dict()
 
-        skey = get_random_bytes(32)
-        print ("Key: "+str(skey))
+            # Wait until two users connect, meaning they want to communicate
+            while users_connected < 2:
+                conn, _ = s.accept()
+                conns.append(conn)
 
-        cipher = AES.new(get_key(data["idp"]), AES.MODE_GCM, nonce=data["rp"])
-        cipher.update(data["idp"])
-        Cp,s = cipher.encrypt_and_digest(skey)
+                p = Process(target=communication_request, args=(conn, users_connected, data))
+                processes.append(p)
+                p.start()
 
-        cipher = AES.new(get_key(data["idq"]), AES.MODE_GCM, nonce=data["rp"])
-        cipher.update(data["idq"])
-        Cq,s = cipher.encrypt_and_digest(skey)
+                users_connected += 1
+            
+            # 1. 2. Wait for both users to send their IDs and nonces
+            for p in processes:
+                p.join()
 
-        #proceso de MAC
-        cobj = CMAC.new(get_keym(data["idp"]), ciphermod=AES)
-        cobj.update(bytearray(data["idq"]+data["rq"]+data["rp"]+Cp))
-        Tp=cobj.digest()
+            
+            # 3.1. Generate the secret key
+            skey = get_random_bytes(32)
+            print ("Key: "+str(skey))
 
-        cobj = CMAC.new(get_keym(data["idq"]), ciphermod=AES)
-        cobj.update(bytearray(data["idp"]+data["rq"]+data["rp"]+Cq))
-        Tq=cobj.digest()
+            # 3.2. Encrypt secret key with P's key
+            cipher = AES.new(get_key(data["idp"]), AES.MODE_GCM, nonce=data["rp"])
+            cipher.update(data["idp"])
+            Cp,_ = cipher.encrypt_and_digest(skey)
 
-        conns[0].sendall(str.encode("\n".join([
-                str(int.from_bytes(Cp, "little")),
-                str(int.from_bytes(Tp, "little")),
-                str(int.from_bytes(data["idq"], "little")),
-                str(int.from_bytes(data["rq"], "little")),
-                str(0)
-        ])))
-        conns[1].sendall(str.encode("\n".join([
-                str(int.from_bytes(Cq, "little")),
-                str(int.from_bytes(Tq, "little")),
-                str(int.from_bytes(data["idp"], "little")),
-                str(int.from_bytes(data["rp"], "little")),
-                str(1)
-        ])))
+            # 3.3. Encrypt secret key with Q's key
+            cipher = AES.new(get_key(data["idq"]), AES.MODE_GCM, nonce=data["rp"])
+            cipher.update(data["idq"])
+            Cq,_ = cipher.encrypt_and_digest(skey)
+
+            # 3.4. Calculate Tp <- MACp(idq, rp, rq, cp)
+            cobj = CMAC.new(get_keym(data["idp"]), ciphermod=AES)
+            cobj.update(bytearray(data["idq"]+data["rq"]+data["rp"]+Cp))
+            Tp=cobj.digest()
+
+            # 3.5. Calculate Tq <- MACq(idp, rp, rq, cq)
+            cobj = CMAC.new(get_keym(data["idq"]), ciphermod=AES)
+            cobj.update(bytearray(data["idp"]+data["rq"]+data["rp"]+Cq))
+            Tq=cobj.digest()
+
+            # 4. Send information to P
+            conns[0].sendall(str.encode("\n".join([
+                    str(int.from_bytes(Cp, "little")),
+                    str(int.from_bytes(Tp, "little")),
+                    str(int.from_bytes(data["idq"], "little")),
+                    str(int.from_bytes(data["rq"], "little")),
+                    str(0)
+            ])))
+
+            # 4. Send information to Q
+            conns[1].sendall(str.encode("\n".join([
+                    str(int.from_bytes(Cq, "little")),
+                    str(int.from_bytes(Tq, "little")),
+                    str(int.from_bytes(data["idp"], "little")),
+                    str(int.from_bytes(data["rp"], "little")),
+                    str(1)
+            ])))
+            # TTP finished its job
        
 
 def add_user(pair):
+    """Add a new user to the table"""
     global users
     with open("./users.json") as users_file:
         users = json.load(users_file)
@@ -138,19 +163,24 @@ def add_user(pair):
         users_file.write(json.dumps(users))
 
 def update_users():
+    """Reload users information"""
     global users
     with open("./users.json") as users_file:
         users = json.load(users_file)
 
 def main():
+    # Load users information
     with open("./users.json") as users_file:
         global users
         users = json.load(users_file)
-    register_thread = Process(target=register_requests_handler)
-    register_thread.start()
 
-    asdf_thread = Process(target=asdf_handler)
-    asdf_thread.start()
+    # Start a process to handle registration requests
+    register_process = Process(target=register_requests_handler)
+    register_process.start()
+
+    # Start a process to handle communication requests
+    communication_request_process = Process(target=communication_requests_handler)
+    communication_request_process.start()
 
 main()
     
